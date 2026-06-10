43ed60b21d0b51c56da3c71a17e2a52263df9784
Cemrenur KELES <cemrenur.keles@etu.unistra.fr>
2026-06-10 17:42:54 +0200

    Update languages page asset paths

--- langues.php
+++ langues.php
@@ -10,8 +10,8 @@
       href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700;800&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="./css/commun.css" />
-    <link rel="stylesheet" href="./css/langues.css" />
+    <link rel="stylesheet" href="./assets/css/commun.css" />
+    <link rel="stylesheet" href="./assets/css/langues.css" />
   </head>
   <body data-page="langues">
     <header></header>
