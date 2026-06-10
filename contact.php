5098e13acaebdf0c3c81c8dbd1910dfb44f819b7
Cemrenur KELES <cemrenur.keles@etu.unistra.fr>
2026-06-10 17:38:56 +0200

    Add contact page

---
+++ contact.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contact | CV de Cemrenur Keles</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700;800&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="./css/commun.css" />
+    <link rel="stylesheet" href="./css/contact.css" />
+  </head>
+  <body data-page="contact">
+    <header></header>
+    <main>
+      <section>
+        <header>
+          <p>Contact</p>
+          <h1>Me joindre</h1>
+        </header>
+        <div>
+          <article>
+            <h2>Coordonnees</h2>
+            <p>Rue Henri Loux, 67200 Strasbourg</p>
+            <p>cemrenurkeles@gmail.com</p>
+            <p>06 66 14 93 26</p>
+            <p>Permis B</p>
+          </article>
+        </div>
+      </section>
+    </main>
+    <script src="./js/app.js"></script>
+  </body>
+</html>
