1b46eb99936ef7c153c780fd173af035ae6dff50
Cemrenur KELES <cemrenur.keles@etu.unistra.fr>
2026-06-10 17:38:57 +0200

    Add education page

---
+++ formation.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Formation | CV de Cemrenur Keles</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700;800&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="./css/commun.css" />
+    <link rel="stylesheet" href="./css/formation.css" />
+  </head>
+  <body data-page="formation">
+    <header></header>
+    <main>
+      <section>
+        <header>
+          <p>Formation</p>
+          <h1>Mon parcours universitaire et scolaire</h1>
+        </header>
+        <div>
+          <article>
+            <p><strong>Depuis 2022</strong></p>
+            <h2>Licence en informatique</h2>
+            <p>Universite de Strasbourg</p>
+          </article>
+          <article>
+            <p><strong>2019 - 2022</strong></p>
+            <h2>Bac general</h2>
+            <p>Lycee des Pontonniers, Strasbourg</p>
+          </article>
+        </div>
+      </section>
+    </main>
+    <script src="./js/app.js"></script>
+  </body>
+</html>
