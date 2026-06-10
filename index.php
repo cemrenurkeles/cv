10c47cbefcdd7431cc244d1e8205841594fc35b3
Cemrenur KELES <cemrenur.keles@etu.unistra.fr>
2026-06-10 17:42:54 +0200

    Update home page asset paths

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
       href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700;800&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="./css/commun.css" />
-    <link rel="stylesheet" href="./css/index.css" />
+    <link rel="stylesheet" href="./assets/css/commun.css" />
+    <link rel="stylesheet" href="./assets/css/index.css" />
   </head>
   <body data-page="home">
     <header></header>
